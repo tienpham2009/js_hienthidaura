7221f1b5e868d4035900fb49136759c4a8fc6623
tienpham2009 <tiendx2009@gmail.com>
2021-04-06 21:22:48 +0700

    hien thi dau ra

---
+++ hienthidaura.js
@@ -0,0 +1,12 @@
+function myfuntion() {
+    alert("Xin Chào");
+}
+function message() {
+    document.getElementById("message").innerHTML="Xin Chào";
+}
+function showadd() {
+    document.write("hello")
+}
+function addshow() {
+    console.log("hi!")
+}
